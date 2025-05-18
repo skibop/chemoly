abc982f6f8adfb9dbb5a1d98c3b444775a16452e
Ankit Kale <kaleankit7979@outlook.com>
2025-05-17 20:49:43 -0400

    change

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,11 +21,19 @@ module.exports = {
     rules: {
       'react/react-in-jsx-scope': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',  // global off, can be kept or removed
       '@typescript-eslint/no-explicit-any': 'off',
       'react/no-unescaped-entities': 'off',
       'import/no-unresolved': 'off',
       '@typescript-eslint/no-empty-interface': 'off',
     },
+    overrides: [
+      {
+        files: ['src/app/page.tsx', 'src/app/types/page.tsx'],
+        rules: {
+          '@typescript-eslint/no-unused-vars': 'off',
+        },
+      },
+    ],
   };
   
